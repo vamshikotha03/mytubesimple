129a3ec0c24c9e9332fe202989d98aff1a4a41c8
Vamshi Reddy <vamshikotha03@gmail.com>
2023-06-02 10:44:14 -0400

    Initial commit

---
+++ charger.js
@@ -0,0 +1,10 @@
+// Function to calculate the square of a number
+function calculateSquare(number) {
+    return number * number;
+  }
+  
+  // Usage example
+  var num = 5;
+  var square = calculateSquare(num);
+  console.log("The square of " + num + " is: " + square);
+  
