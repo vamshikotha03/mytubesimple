0d34d8c94ca1990d7471dc04ff06b8471b9cfd44
Vamshi Reddy <vamshikotha03@gmail.com>
2023-06-02 10:51:11 -0400

    changed charger.js

--- charger.js
+++ charger.js
@@ -1,10 +1,1 @@
-// Function to calculate the square of a number
-function calculateSquare(number) {
-    return number * number;
-  }
-  
-  // Usage example
-  var num = 5;
-  var square = calculateSquare(num);
-  console.log("The square of " + num + " is: " + square);
-  
+console.log('Hello');
